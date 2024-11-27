a28e10c8f6f4659511cdf990712137a5d72798f3
ChauhanAaditya <caaditya2003@gmail.com>
2024-11-27 15:14:48 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { Loader } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { Suspense } from "react";
+import { Experience } from "./components/Experience";
+import { UI } from "./components/UI";
+
+function App() {
+  return (
+    <>
+      <button>Ok</button>
+      <UI />
+      <Loader />
+      <Canvas shadows camera={{ position: [-0.5, 1, 4], fov: 45 }}>
+        <group position-y={0}>
+          <Suspense fallback={null}>
+            <Experience />
+          </Suspense>
+        </group>
+      </Canvas>
+    </>
+  );
+}
+
+export default App;
